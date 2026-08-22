bebb99d092f3e87f51d439865caa6946be4260fb
Yerzat <your_email@example.com>
2026-08-22 23:46:09 +0500

    fix congif

--- app/config.py
+++ app/config.py
@@ -3,12 +3,11 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     database_url: str
-    SECRET_KEY : SecretStr
+    secret_key: SecretStr
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
 
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8"
-    )
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
